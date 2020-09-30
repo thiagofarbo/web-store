e23589febddd21ec0e26c15ea40f2cb76a4c6b26
Thiago Emidio <thiago.augusto.ext@sodexo.com>
2020-09-30 15:02:40 -0300

    First commit

---
+++ src/test/java/com/webstore/NativeWebStoreResourceIT.java
@@ -0,0 +1,9 @@
+package com.webstore;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeWebStoreResourceIT extends WebStoreResourceTest {
+
+    // Execute the same tests but in native mode.
+}
